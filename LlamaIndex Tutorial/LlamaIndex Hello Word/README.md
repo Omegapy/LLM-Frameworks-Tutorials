cc96fc8e52a7e3d78a4933b2553bb95ec611f3e6
Alex Ricciardi <alex.omegapy@gmail.com>
2024-01-03 10:23:34 -0700

    Added tutorial LlamaIdex Hello Word

---
+++ LlamaIndex Tutorial/LlamaIndex Hello Word/README.md
@@ -0,0 +1,41 @@
+-----------------------------------------------------------------------------------------------------------------------------
+LlamaIndex Hello Word
+-----------------------------------------------------------------------------------------------------------------------------
+
+ Alejandro Ricciardi (Omegapy)  
+ created date: 01/03/2024 
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+Projects Description:  
+- Load WebPage (dataConnector)
+- Store it in an idex
+- Create a query Engine
+- Ask question about the webpage
+- Use the LlamaIndex fuction:
+    - SimpleWebReader
+    - VectoreStoreIndex
+    - QueryEngine
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+Requirements:  
+- [Python](https://www.python.org/)   
+- [LlamaIndex](https://www.llamaindex.ai/)  
+- [OpenAI API](https://openai.com/)  
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+My Links:   
+- [GitHub](https://github.com/Omegapy)   
+- [Facebook](https://www.facebook.com/profile.php?id=100089638857137)  
+- [Twitter](https://twitter.com/RicciardiAlex)   
+- [Instagram](https://www.instagram.com/alexomegapy/)   
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+
+
+
---
+++ LlamaIndex Tutorial/LlamaIndex Hello Word/main.py
@@ -0,0 +1,53 @@
+#----------------------------------------------------------------------------
+#                         LlamaIndex Hello Word
+# ---------------------------------------------------------------------------
+#
+#  Alejandro Ricciardi (Omegapy)
+#  created date: 01/03/2024
+#
+# --------------------------------------------------------------------------
+#
+# Projects Description:
+# - Load WebPage (dataConnector)
+# - Store it in an index
+# - Create a query Engine
+# - Ask question about the webpage
+# - Use the LlamaIndex function:
+#     - SimpleWebReader
+#     - VectoreStoreIndex
+#     - QueryEngine
+#----------------------------------------------------------------------------
+
+#--------------------------------------
+#            Dependencies
+#--------------------------------------
+#----- LlamaIndex
+from llama_index.readers import SimpleWebPageReader
+from llama_index import VectorStoreIndex
+#----- APY Key
+import os
+from dotenv import load_dotenv,find_dotenv
+load_dotenv(find_dotenv())
+OPENAI_API_KEY = os.environ.get("OPEN_AI_KEY")
+
+#--------------------------------------
+#            Functions
+#--------------------------------------
+#---- Main
+def main(url:str) -> None:
+    documents = SimpleWebPageReader(html_to_text=True).load_data(urls=[url]) # needs pipenv install html2text
+    index = VectorStoreIndex.from_documents(documents=documents) # Vector store
+    query_engine = index.as_query_engine() # using the vector store as a query engine
+    # Querying the engine (vector store) with a prompt, LLM returning a response
+    # Note: by default the OpenAI model is chatgpt3.5
+    response = query_engine.query("Summarize")
+    print(f"\n{response}")
+
+#--------------------------------------
+#            Main Program
+#--------------------------------------
+if __name__ == '__main__':
+    print("\nHello Word!")
+    main(url='https://www.datastax.com/guides/what-is-llamaindex')
+
+
